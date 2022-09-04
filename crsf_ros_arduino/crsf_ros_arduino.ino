018b7089813f00b9c874cff4f59ad35614bd0b6b
unknown <rakinuzzaman@gmail.com>
2022-09-05 03:50:49 +0600

    added code

---
+++ crsf_ros_arduino/crsf_ros_arduino.ino
@@ -0,0 +1,40 @@
+/*
+This example outputs values from all PPM channels to Serial
+in a format compatible with Arduino IDE Serial Plotter
+*/
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int16MultiArray.h>
+#include <PPMReader.h>
+
+ros::NodeHandle  nh;
+std_msgs::Int16MultiArray int_msg;
+ros::Publisher rc_signal("rc_signal", &int_msg);
+
+int16_t ch[8];
+// Initialize a PPMReader on digital pin 3 with 6 expected channels.
+byte interruptPin = 2;
+byte channelAmount = 8;
+PPMReader ppm(interruptPin, channelAmount);
+
+void setup() {
+    Serial.begin(115200);
+//    nh.initNode();
+//    nh.advertise(rc_signal);
+}
+
+void loop() {
+  
+    // Print latest valid values from all channels
+    for (byte channel = 1; channel <= channelAmount; ++channel) {
+        ch[channel - 1] = ppm.latestValidChannelValue(channel, 0);
+        Serial.print(ch[channel - 1]);
+        if(channel < channelAmount) Serial.print('\t');
+    }
+    Serial.println();
+//    int_msg.data = ch;
+//    int_msg.data_length = 9;
+//    rc_signal.publish(&int_msg);
+//    nh.spinOnce();
+    delay(20);
+}
